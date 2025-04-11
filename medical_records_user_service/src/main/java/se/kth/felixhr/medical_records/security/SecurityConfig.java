752683693ad13c35060dc94465c34b3e217ea105
Felhen <102024963+Felhen@users.noreply.github.com>
2025-04-11 18:38:41 +0200

    uugh

--- medical_records_user_service/src/main/java/se/kth/felixhr/medical_records/security/SecurityConfig.java
+++ medical_records_user_service/src/main/java/se/kth/felixhr/medical_records/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package se.kth.felixhr.medical_records.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -28,6 +29,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/login", "/register", "/user", "/users", "/patient", "/patient/{id}", "/patients").permitAll()
                         .anyRequest().authenticated()
                 )
